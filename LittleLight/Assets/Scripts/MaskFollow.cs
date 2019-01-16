34e6c8a35e6c2e75162afc6e1211b9b734dea79f
Jarell Hackett <Jarellh94@gmail.com>
2019-01-16 18:26:26 -0500

    Removed lighting changes. Added a sprite mask to player and light bullets.
    Will work on trying to use a sprite mask to reveal what's in the darkness instead of using light itself. I'll play around with this idea before trying to use light.

---
+++ LittleLight/Assets/Scripts/MaskFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    public float shrinkSpeed = .5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y + 9, playerTransform.position.z - 9);
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            Vector3 newScale = transform.localScale;
+            newScale.x += shrinkSpeed * Time.deltaTime;
+            newScale.y += shrinkSpeed * Time.deltaTime;
+
+            transform.localScale = newScale;
+        }
+
+        if (Input.GetKey(KeyCode.K))
+        {
+            Vector3 newScale = transform.localScale;
+            newScale.x -= shrinkSpeed * Time.deltaTime;
+            newScale.y -= shrinkSpeed * Time.deltaTime;
+
+            transform.localScale = newScale;
+        }
+    }
+}
